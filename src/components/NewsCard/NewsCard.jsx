8f72c4543df484866c6ed17cc5efcb971bdd8165
MariaAddict <mashakan27@gmail.com>
2021-02-15 00:29:49 +0700

    fix: исправить стили NewsCard с многоточием

--- src/components/NewsCard/NewsCard.jsx
+++ src/components/NewsCard/NewsCard.jsx
@@ -1,21 +1,17 @@
 import "./NewsCard.css";
-import { useState } from 'react';
+import React, { useState, useEffect, createRef } from "react";
 
-function NewsCard({ card, mainTheme}) {
-    const [saveIsClick, setSaveIsClick] = useState(false);
-  
-    function handleSaveClick() {
-        saveIsClick ? setSaveIsClick(false) : setSaveIsClick(true);
-    }
+function NewsCard({ card, mainTheme }) {
+  const [saveIsClick, setSaveIsClick] = useState(false);
+
+  function handleSaveClick() {
+    saveIsClick ? setSaveIsClick(false) : setSaveIsClick(true);
+  }
 
   return (
     <li className="cards__item">
       <a href={card.link} className="cards__link">
-        <img
-          src={card.image}
-          alt={card.title}
-          className="cards__image"
-        />
+        <img src={card.image} alt={card.title} className="cards__image" />
         <div className="cards__description">
           <p className="cards__date">{card.date}</p>
           <h4 className="cards__title">{card.title}</h4>
@@ -24,14 +20,29 @@ function NewsCard({ card, mainTheme}) {
         <p className="cards__source">{card.source}</p>
         <button
           type="button"
-          className={`cards__button-save ${mainTheme ? "cards__button-save_theme_main" : "cards__button-save_theme_save-news"}`}
+          className={`cards__button-save ${
+            mainTheme
+              ? "cards__button-save_theme_main"
+              : "cards__button-save_theme_save-news"
+          }`}
           onClick={handleSaveClick}
         ></button>
         <button
-        type="button" className={`cards__button-no-save ${saveIsClick ? "" :"cards__button-no-save_hidden" }`} >
-           {/* onClick={handleSaveClick} */}
-           { mainTheme ? "Войдите, чтобы сохранять статьи" : "Убрать из сохранённых" }</button>
-        {!mainTheme && (<div className="cards__keyword"><p className="cards__keyword-text">{card.keyword}</p></div>)}
+          type="button"
+          className={`cards__button-no-save ${
+            saveIsClick ? "" : "cards__button-no-save_hidden"
+          }`}
+        >
+          {/* onClick={handleSaveClick} */}
+          {mainTheme
+            ? "Войдите, чтобы сохранять статьи"
+            : "Убрать из сохранённых"}
+        </button>
+        {!mainTheme && (
+          <div className="cards__keyword">
+            <p className="cards__keyword-text">{card.keyword}</p>
+          </div>
+        )}
       </a>
     </li>
   );
